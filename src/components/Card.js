bf8f4deb9dee64fd545b2a126ff85cc62dd66611
AMHBI <134843068+AMHBI@users.noreply.github.com>
2023-07-27 20:37:10 +0330

    Adding counter to products and search text

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { Component } from "react";
 import styles from "./Card.module.css";
+import up from "../images/up.png";
+import down from "../images/down.png";
 
-const Card = (props) => {
-    const {image ,name , price} = props;
-    
+class Card extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+  }
+  downHandler = () => {
+    if (this.state.counter >= 1) {
+      this.setState((prevState) => ({
+        counter: prevState.counter - 1,
+      }));
+    }
+  };
+  upHandler = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+
+
+  render() {
+    const { image, name, price } = this.props;
+    const counter = this.state.counter;
     return (
-        <div className={styles.container}>
-            <img src={image}/>
-            <h3>{name}</h3>
-            <p>{price} $</p>
-            <button>Add To Cart</button>
+      <div className={styles.container}>
+        <img src={image} />
+        <h3>{name}</h3>
+        <p>{price} $</p>
+        <div className={styles.counter}>
+          <img src={down} alt='arrow' className={this.state.counter ? "": styles.deactived} onClick={this.downHandler} />
+          <span>{counter}</span>
+          <img src={up} alt='arrow' onClick={this.upHandler} />
         </div>
+        <button>Add To Cart</button>
+      </div>
     );
-};
+  }
+}
 
 export default Card;
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import styles from "./Search.module.css";
 
 class Search extends Component {
-    render() {
+    constructor(){
+        super()
+        this.state = {
+            text : "",
+        };
+    }
+    changeHandler = event => {
+        this.setState({
+            text : event.target.value,
+        })
+    }
+        render() {
+        const {text} = this.state;
         return (
+            
             <div className={styles.container}>
                 <p>Search what you want</p>
-                <input placeholder='Search...'/>
+                <input placeholder='Search...' value={this.state.text} onChange={this.changeHandler}/>
+                <span>{text}</span>
             </div>
+                
         );
     }
 }
