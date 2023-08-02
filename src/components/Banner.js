185f84cc9224d4c259c229224659d49e9dc7b7ec
AMHBI <134843068+AMHBI@users.noreply.github.com>
2023-08-02 23:36:29 +0330

    Make all component (Class Component)

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React , {Component} from 'react';
 import styles from "./Banner.module.css";
 import banner from "../images/Banner.jpg";
 
-const Banner = () => {
-    return (
-        <div className={styles.container}>
+class Banner extends Component {
+    render() {
+        return (
+            <div className={styles.container}>
             <img src={banner} alt= "" />
             <div className={styles.textContainer}>
                 <h1>Online Shop</h1>
                 <p>Buy <span> Online </span> your needs </p>
             </div>
         </div>
-    );
-};
+        );
+    }
+}
 
 export default Banner;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from "./Footer.module.css";
 
-const Footer = () => {
-    return (
-        <div className={styles.container}>
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className={styles.container}>
             <p>
                 &copy; All right reserved 2021
             </p>
         </div>
-    );
-};
+        );
+    }
+}
 
 export default Footer;
